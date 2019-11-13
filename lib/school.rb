67e84331226c67a65b5f23d7acb965249b361bb5
Junaid Malik <jmalik981@gmail.com>
2019-11-13 04:25:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,17 @@
-school = School.new("Bayside High School")
-school.roster
+class School 
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    roster|grade| ||=[]
+    roster|grade| << student_name
+  end
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+end
